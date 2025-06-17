57aa4bbd6843805b7efc989692d873a01dda3308
Vincent Berthiaume <vberthiaume@gmail.com>
2025-06-17 10:00:18 -0400

    cleanup

--- scripts/nbody.cpp
+++ scripts/nbody.cpp
@@ -159,7 +159,6 @@ void iteration_fork_union_dynamic(fun::thread_pool_t &pool, body_t *_FU_RESTRICT
 #pragma endregion - Backends
 
 int main() {
-#if 1
     // Read env vars
     std::size_t n = std::stoul(std::getenv("NBODY_COUNT") ?: "0");
     std::size_t const iterations = std::stoul(std::getenv("NBODY_ITERATIONS") ?: "1000");
@@ -218,5 +217,4 @@ int main() {
 
     std::fprintf(stderr, "Unsupported backend: %s\n", backend.data());
     return EXIT_FAILURE;
-#endif
 }
--- scripts/test.cpp
+++ scripts/test.cpp
@@ -231,7 +231,6 @@ static bool stress_test_composite(std::size_t const threads_count, std::size_t c
 }
 
 int main() {
-#if 1
     using test_func_t = bool() /* noexcept */;
     struct {
         char const *name;
@@ -304,6 +303,5 @@ int main() {
         return EXIT_FAILURE;
     }
     std::printf("All %zu stress tests passed\n", total_stress_tests);
-#endif
     return EXIT_SUCCESS;
 }
